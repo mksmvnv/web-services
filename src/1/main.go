91871b984899f1a27cbcdf9f415893a4b588e7b7
Maksim <mksmvnv@icloud.com>
2025-01-11 23:44:50 +0300

    Update recursive processing conditions

--- src/1/main.go
+++ src/1/main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
-
-type Item struct {
-	Name string
-}
 
 func main() {
 	out := os.Stdout
@@ -24,20 +21,30 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	var buf []Item
 	items, err := os.ReadDir(path)
 	if err != nil {
 		return err
 	}
 	for _, item := range items {
 		if printFiles {
-			buf = append(buf, Item{item.Name()})
-		} else {
 			if item.IsDir() {
-				buf = append(buf, Item{item.Name()})
+				fmt.Fprintln(out, item.Name())
+				subDir := filepath.Join(path+"/", item.Name())
+				dirTree(out, subDir, printFiles)
+			} else {
+				fileSize, err := os.Stat(item.Name())
+				if err != nil {
+					return err
+				}
+				fmt.Fprintf(out, "%s (%db)\n", item.Name(), fileSize.Size())
 			}
+
+		}
+		if !printFiles && item.IsDir() {
+			fmt.Fprintln(out, item.Name())
+			subDir := filepath.Join(path+"/", item.Name())
+			dirTree(out, subDir, printFiles)
 		}
 	}
-	fmt.Println(buf)
 	return nil
 }
