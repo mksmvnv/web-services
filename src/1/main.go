5bcb6fc926d5151f2aecf9dfbadb73bc22c220c5
Maksim <mksmvnv@icloud.com>
2025-01-15 14:55:36 +0300

    Add sort by name and fix prefix bugs

--- src/1/main.go
+++ src/1/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
 )
 
 func main() {
@@ -21,38 +22,110 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	// Read directory
-	fileSystemObjects, err := os.ReadDir(path)
+	// Create a nested function
+	var walk func(string, string) error
+	walk = func(path string, prefix string) error {
+		fileSystemObjects, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+
+		// Sort by name
+		sort.Slice(fileSystemObjects, func(i, j int) bool {
+			return fileSystemObjects[i].Name() < fileSystemObjects[j].Name()
+		})
+
+		// Filter by object type
+		if !printFiles {
+			var dirsOnly []os.DirEntry
+			for _, obj := range fileSystemObjects {
+				if obj.IsDir() {
+					dirsOnly = append(dirsOnly, obj)
+				}
+			}
+			fileSystemObjects = dirsOnly
+		}
+
+		for index, fileSystemObject := range fileSystemObjects {
+			isLast := index == len(fileSystemObjects)-1
+
+			// Ignore .DS_Store MacOS
+			if fileSystemObject.Name() == ".DS_Store" {
+				continue
+			}
+
+			if fileSystemObject.IsDir() {
+				printDir(out, fileSystemObject, prefix, isLast)
+				subDir := filepath.Join(path, fileSystemObject.Name())
+				// Add recursive call
+				if isLast {
+					if err := walk(subDir, prefix+"\t"); err != nil {
+						return err
+					}
+				} else {
+					if err := walk(subDir, prefix+"│\t"); err != nil {
+						return err
+					}
+				}
+			} else if printFiles {
+				// Print files
+				if err := printFile(out, fileSystemObject, prefix, isLast); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}
+
+	return walk(path, "")
+}
+
+// Print directories
+func printDir(
+	out io.Writer,
+	fileSystemObject os.DirEntry,
+	prefix string, isLast bool,
+) {
+	if isLast {
+		fmt.Fprintf(out, "%s└───%s\n", prefix, fileSystemObject.Name())
+	} else {
+		fmt.Fprintf(out, "%s├───%s\n", prefix, fileSystemObject.Name())
+	}
+}
+
+// Print files
+func printFile(
+	out io.Writer,
+	fileSystemObject os.DirEntry,
+	prefix string,
+	isLast bool,
+) error {
+	fileInfo, err := fileSystemObject.Info()
+
 	if err != nil {
 		return err
 	}
-	for _, fileSystemObject := range fileSystemObjects {
-		// If we use -f argument
-		if printFiles {
-			if fileSystemObject.IsDir() {
-				fmt.Fprintf(out, "%s\n", fileSystemObject.Name())
-				subDir := filepath.Join(path, fileSystemObject.Name())
-				dirTree(out, subDir, printFiles)
-			} else {
-				const emptySize int64 = 0
-				fileSize, err := os.Stat(filepath.Join(path, fileSystemObject.Name()))
-				if err != nil {
-					return err
-				}
-				// If file is empty
-				if fileSize.Size() == emptySize {
-					fmt.Fprintf(out, "%s (empty)\n", fileSystemObject.Name())
-					continue
-				}
-				fmt.Fprintf(out, "%s (%db)\n", fileSystemObject.Name(), fileSize.Size())
-			}
+
+	// If file is empty
+	if fileInfo.Size() == 0 {
+		if isLast {
+			fmt.Fprintf(out, "%s└───%s (empty)\n", prefix, fileSystemObject.Name())
+		} else {
+			fmt.Fprintf(out, "%s├───%s (empty)\n", prefix, fileSystemObject.Name())
 		}
-		// If we don't use -f argument
-		if !printFiles && fileSystemObject.IsDir() {
-			fmt.Fprintf(out, "%s\n", fileSystemObject.Name())
-			subDir := filepath.Join(path, fileSystemObject.Name())
-			dirTree(out, subDir, printFiles)
-		}
+		return nil
+	}
+
+	// If file is not empty
+	if isLast {
+		fmt.Fprintf(
+			out, "%s└───%s (%db)\n", prefix, fileSystemObject.Name(), fileInfo.Size(),
+		)
+	} else {
+		fmt.Fprintf(
+			out, "%s├───%s (%db)\n", prefix, fileSystemObject.Name(), fileInfo.Size(),
+		)
 	}
 	return nil
 }
