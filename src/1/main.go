cb2ab484307e0c6116e08562c18b5e4fbc8bea86
Maksim <mksmvnv@icloud.com>
2025-01-15 19:53:14 +0300

    Fix some bugs

--- src/1/main.go
+++ src/1/main.go
@@ -8,19 +8,6 @@ import (
 	"sort"
 )
 
-func main() {
-	out := os.Stdout
-	if !(len(os.Args) == 2 || len(os.Args) == 3) {
-		panic("usage go run main.go . [-f]")
-	}
-	path := os.Args[1]
-	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	err := dirTree(out, path, printFiles)
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
 func dirTree(out io.Writer, path string, printFiles bool) error {
 	// Create a nested function
 	var walk func(string, string) error
@@ -128,4 +115,18 @@ func printFile(
 		)
 	}
 	return nil
+}
+
+// Main
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
 }
